de8e1c388018c0a3dd617b7f67b874ed30f21b25
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2024-03-22 20:00:26 -0700

    Using Puppet to install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# install puppet-lint -v 2.5.0
+# Puppet manifest to install Flask version 2.1.0 using pip3
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/local/bin', '/usr/bin'],
+  require => Package['python3-pip'],
 }
 
