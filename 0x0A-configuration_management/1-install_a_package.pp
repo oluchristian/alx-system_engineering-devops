50eefcec80adccf9af85fa5993bf483bc232e8fd
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2024-03-22 20:08:35 -0700

    Using Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,20 @@
-# Puppet manifest to install Flask version 2.1.0 using pip3
+#!/usr/bin/puppet
+
+# Puppet manifest to install Flask version 2.1.0 and Werkzeug version 2.1.1 using pip3
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
 
 package { 'python3-pip':
   ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/local/bin', '/usr/bin'],
-  require => Package['python3-pip'],
 }
 
