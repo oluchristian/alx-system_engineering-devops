00123b77ab948fb13ec59272decc56269a916d48
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-04-27 07:04:20 +0100

    formatted the code to comply with pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
     url = f"https://jsonplaceholder.typicode.com/todos?userId={employeeId}"
     response = requests.get(url)
     todos = response.json()
-    todo_number = len(todos)
-    task_completed = sum(1 for todo in todos if todo.get("completed"))
-    print(f"Employee {employeeId} is done with tasks({task_completed}/{todo_number}):")
+    todo_num = len(todos)
+    completed = sum(1 for todo in todos if todo.get("completed"))
+    print(f"Employee {employeeId} is done with tasks({completed}/{todo_num}):")
     for todo in todos:
         if todo['completed']:
             print(f"\t{todo['title']}")
