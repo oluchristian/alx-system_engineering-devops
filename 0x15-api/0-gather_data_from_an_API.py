4a39f7b7cc466c108cb6ecefc4656ea5d11685c2
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-04-27 19:30:10 +0100

    python script that display todo list progress for a given employee ID

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,12 +12,15 @@ if __name__ == "__main__":
         employeeId = int(sys.argv[1])
     except ValueError:
         sys.exit(1)
-    url = f"https://jsonplaceholder.typicode.com/todos?userId={employeeId}"
-    response = requests.get(url)
-    todos = response.json()
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employeeId}"
+    todo_url = user_url + "/todos"
+    r_user = requests.get(user_url)
+    user = r_user.json()
+    r_todos = requests.get(todo_url)
+    todos = r_todos.json()
     todo_num = len(todos)
     completed = sum(1 for todo in todos if todo.get("completed"))
-    print(f"Employee {employeeId} is done with tasks({completed}/{todo_num}):")
+    print(f"Employee {user.get('name')} is done with tasks({completed}/{todo_num}):")
     for todo in todos:
         if todo['completed']:
             print(f"\t{todo['title']}")
