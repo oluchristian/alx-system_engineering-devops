1ad3da6ba3ea6ac2284c00fb0e0f223d7ccadd58
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-04-28 03:19:48 +0100

    Python script to export data in the CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -17,20 +17,14 @@ if __name__ == "__main__":
     todo_url = user_url + "/todos"
     r_user = requests.get(user_url)
     user = r_user.json()
-    user_name = user.get('name')
+    user_name = user.get('username')
     r_todos = requests.get(todo_url)
     todos = r_todos.json()
-    todo_num = len(todos)
-    completed = sum(1 for todo in todos if todo.get("completed"))
-    print(f"Employee {user_name} is done with tasks({completed}/{todo_num}):")
-    for todo in todos:
-        if todo['completed']:
-            print(f"\t{todo['title']}")
     csv_file = f"{employeeId}.csv"
     with open(csv_file, mode='w', newline='') as file:
         writer = csv.writer(file)
         attr = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
         writer.writerow(attr)
-        input = [employeeId, user_name, todo["completed"], todo["title"]]
         for todo in todos:
-            writer.writerow(input)
+            row = [todo.get("userId"), user_name, todo.get("completed"), todo.get("title")]
+            writer.writerow(row)
